eb95f2408e09be41a801abb701b35732789546c0
Abhishek sahu <104501813+Abhisheksahu12@users.noreply.github.com>
2022-12-17 08:24:29 +0530

    Create Binarysearch.py

---
+++ Binarysearch.py
@@ -0,0 +1,15 @@
+def binarysearch(arr,key):
+       I=0
+       E=len(arr)-1
+       While(l<E):
+             Mid=(I+E)/2
+             if(arr[mid]==key):
+                    return mid
+                Elif(arr[mid]>key):
+                     E=mid-1
+            Else:
+                     L=mid+1
+
+
+
+Return -1
